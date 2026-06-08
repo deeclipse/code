0f1bf93711ca791d3e123f7dff9e9e90f7a6e2cc
OLFMSQL <kokswees@gmail.com>
2026-06-08 13:28:45 +0200

    Warehouse in c programming

--- playground.c
+++ playground.c
@@ -1,9 +1,31 @@
 #include <stdio.h>
+#include <string.h>
 
-int main() {
-
+int main() 
+{
     // Writing print statement to print hello world
     printf("Hello World");
-
     return 0;
 }
+//
+// char checkIfItemNumberExists(char itemNumber) {
+//     
+//     char ItemToReturn[20] = "";
+//
+//   if (itemNumber == '\n') {  
+//       typedef struct
+//       {
+//           int items[10];
+//           int count;
+//       } ItemContainer;
+//       
+//       while (ItemToReturn[0] == '\0') {
+//           for (int i = 0; i < itemCount; i++) {
+//               if (items[i].itemNumber == itemNumber) {
+//                   strcpy(ItemToReturn, "INVALID");
+//                   break;
+//               }
+//           }
+//       }
+//   }
+// }
